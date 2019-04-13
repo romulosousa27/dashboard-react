502bae83676ef97c4854615e170425a8b5e0eb45
Romulo Sousa <romulosousa27@hotmail.com>
2019-04-13 17:44:49 -0300

    Incorporando o novo componente,

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
+import Resumo from './components/Resumo/Resumo'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <NavBar/>
+                <Resumo/>
             </div>
         );
     }
