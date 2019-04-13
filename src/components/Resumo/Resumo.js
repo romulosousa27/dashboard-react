9f39a07117439dbfad7527ab3c071da8020450a0
Romulo Sousa <romulosousa27@hotmail.com>
2019-04-13 17:44:22 -0300

    Component Resumo

---
+++ src/components/Resumo/Resumo.js
@@ -0,0 +1,97 @@
+import React, {Component} from "react";
+
+class Resumo extends Component {
+    constructor(propos) {
+        super(propos);
+
+        this.state = {
+            consultas: {
+                consultas_30dias_anteiores: 0,
+                consultas_30dias_posteriores: 0
+            },
+            faturamento: {
+                anterior: {
+                    valor: 0,
+                    comparativo: 0
+                },
+                previsao: {
+                    valor: 0,
+                    comparativo: 0
+                }
+            }
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://www.devup.com.br/php/api-dashboard/api")
+            .then(result => result.json().then(data => this.setState(data)));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="mt-2">Resumo</h2>
+
+                <div className="row">
+                    <div className="col">
+                        <h3>Consultas</h3>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header"> 30 dias anteriores</div>
+                                    <div className="card-body">
+                                        {this.state.consultas.consultas_30dias_anteiores}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header"> Proximos 30 dias</div>
+                                    <div className="card-body">
+                                        {this.state.consultas.consultas_30dias_posteriores}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <h3>Faturamento</h3>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header"> 30 dias anteriores</div>
+                                    <div className="card-body">
+                                        {this.state.faturamento.anterior.valor.toLocaleString("pt-BR", {
+                                            style: "currency",
+                                            currency: "BRL"
+                                        })}
+                                        <span className="badge badge-success ml-1">
+                                        {this.state.faturamento.anterior.comparativo}%
+                                    </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header"> 30 dias anteriores</div>
+                                    <div className="card-body">
+                                        {this.state.faturamento.previsao.valor.toLocaleString("pt-BR", {
+                                            style: "currency",
+                                            currency: "BRL"
+                                        })}
+                                        <span className="badge badge-success ml-1">
+                                        {this.state.faturamento.previsao.comparativo}%
+                                    </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Resumo
