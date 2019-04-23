334840f27d7a61b0ddd73e5ace3f7953a113ba01
Romulo Sousa <romulosousa27@hotmail.com>
2019-04-23 20:06:30 -0300

    Criando links de navegação

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,27 @@
 import React, {Component} from "react"
+import {Link} from "react-router-dom";
 
 class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a href="/" className="navbar-brand">Home</a>
+                <Link to="/" className="navbar-brand">Home</Link>
 
                 <button className="navbar-toggler" type="button" data-toogle="collapse" data-target="#navbarNav">
                     <span className="navbar-toggler-icon"> </span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mr-auto"> </ul>
-
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link"> Resumo</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/consultas" className="nav-link"> Consultas</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/faturamento" className="nav-link"> Faturamento</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         )
