e422cce94cb921a545f81288198575e9992af8db
Romulo Sousa <romulosousa27@hotmail.com>
2019-04-23 19:59:18 -0300

    Configurando as rotas.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import NavBar from './components/NavBar/NavBar'
 import Resumo from './components/Resumo/Resumo'
 import Consultas from './components/Consultas/Consultas'
 import Faturamento from  './components/Faturamento/Faturamento'
+import {Switch, Route} from 'react-router-dom';
+
 class App extends Component {
     render() {
         return (
             <div>
                 <NavBar/>
-                <Resumo/>
-                <Consultas/>
-                <Faturamento/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col">
+                            <Switch>
+                                <Route path="/" exact component={Resumo} />
+                                <Route path="/consultas" exact component={Consultas} />
+                                <Route path="/faturamento" exact component={Faturamento} />
+                            </Switch>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
